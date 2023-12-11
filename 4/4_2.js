0387ad6d9e0bfc95138a7044ee5287363763741b
Maxman013 <mc.maxman013@gmail.com>
2023-12-12 03:52:20 +1000

    Finished day 4

--- 4/4_2.js
+++ 4/4_2.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
 let input = fs.readFileSync("4.txt", {encoding: "utf8"}).split('\n');
-let sum = 0;
+let cards = [];
+let counts = {};
 
 for (i = 0; i < input.length; i++) {
+    cards.push(i + 1);
+    counts[i + 1] = counts[i + 1] ? counts[i + 1] + 1 : 1;
+
     let card = input[i].split(": ")[1].split(" | ");
     let winning = card[0].split(' ').filter(item => item != "");
     let numbers = card[1].split(' ').filter(item => item != "");
@@ -10,11 +14,13 @@ for (i = 0; i < input.length; i++) {
     let matches = 0;
     for (j = 0; j < numbers.length; j++) {
         if (winning.includes(numbers[j])) {
-            matches++;
+            let toAdd = ++matches + i + 1;
+            for (k = 0; k < counts[i + 1]; k++) {
+                cards.push(toAdd);
+                counts[toAdd] = counts[toAdd] ? counts[toAdd] + 1 : 1;
+            }
         }
     }
-
-    if (matches > 0) sum += 2 ** (matches - 1);
 }
 
-console.log(sum);
+console.log(cards.length);
